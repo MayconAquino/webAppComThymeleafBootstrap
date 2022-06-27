8ae50cf59b09cddcc887c459389032db136d4126
Maycon de Aquino Oliveira Sousa <maycon.aquino@totvs.com.br>
2022-06-26 22:17:32 -0300

    first commit

---
+++ src/main/java/br/com/aquino/Aquinus/AquinusApplication.java
@@ -0,0 +1,13 @@
+package br.com.aquino.Aquinus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquinusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquinusApplication.class, args);
+	}
+
+}
