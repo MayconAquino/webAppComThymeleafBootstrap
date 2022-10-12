36216d5008e711b9cd04708e2ba6e14d5127a5b6
Maycon Aquino <mayyconaquino@gmail.com>
2022-10-12 13:42:47 -0300

    Implementacao das cores do status do produto

--- src/main/java/br/com/aquino/Aquinus/dto/RequisicaoNovoPedido.java
+++ src/main/java/br/com/aquino/Aquinus/dto/RequisicaoNovoPedido.java
@@ -1,49 +1,73 @@
 package br.com.aquino.Aquinus.dto;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
 import br.com.aquino.Aquinus.model.Pedido;
+import br.com.aquino.Aquinus.model.StatusPedido;
 
 public class RequisicaoNovoPedido {
 
 	private String nomeProduto;
-	
+
 	private String urlProduto;
-	
+
 	private String urlImagem;
-	
+
 	private String descricao;
-	
+
+	@Enumerated(EnumType.STRING)
+	private StatusPedido status;
+
 	public String getNomeProduto() {
 		return nomeProduto;
 	}
+
 	public void setNomeProduto(String nomeProduto) {
 		this.nomeProduto = nomeProduto;
 	}
+
 	public String getUrlProduto() {
 		return urlProduto;
 	}
+
 	public void setUrlProduto(String urlProduto) {
 		this.urlProduto = urlProduto;
 	}
+
 	public String getUrlImagem() {
 		return urlImagem;
 	}
+
 	public void setUrlImagem(String urlImagem) {
 		this.urlImagem = urlImagem;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+
 	}
+
+	public StatusPedido getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusPedido status) {
+		this.status = status;
+	}
+
 	public Pedido toPedido() {
 		Pedido pedido = new Pedido();
 		pedido.setDescricao(descricao);
 		pedido.setNomeProduto(nomeProduto);
 		pedido.setUrlImagem(urlImagem);
 		pedido.setUrlProduto(urlProduto);
+		pedido.setStatus(StatusPedido.AGUARDANDO);
 		return pedido;
 	}
-
-
 
 }
---
+++ src/main/java/br/com/aquino/Aquinus/model/StatusPedido.java
@@ -0,0 +1,5 @@
+package br.com.aquino.Aquinus.model;
+
+public enum StatusPedido {
+	AGUARDANDO, APROVADO, ENTREGUE;
+}
